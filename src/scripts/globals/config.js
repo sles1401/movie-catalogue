04f650a498e063c695572e234792f0f9b5948129
sles1401 <66370267+sles1401@users.noreply.github.com>
2022-11-24 14:54:54 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: 'b12c365bb8ba55a447ccb37c33f2a08a',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
